cf01302b0988dec0fd7aa4ae29f746a40f8c2aa3
Sydney <github@shonx.net>
2024-02-27 01:53:09 -0500

    Add unsubscribe.

---
+++ Pages/Unsubscribe.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace weather_consumer.Pages;
+
+public class UnsubscribeModel : SharedEmailPage
+{
+    private readonly ILogger<UnsubscribeModel> _logger;
+
+    public string? email;
+
+    public UnsubscribeModel(ILogger<UnsubscribeModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public async void OnPost(string? email)
+    {
+        if (email is null)
+        {
+            return;
+        }
+        if(RemoveEmail(email))
+            this.email = email;
+    }
+    public bool RemoveEmail(string? email)
+    {
+        var emails = GetEmails();
+        bool removed = emails.Remove(email);
+        if(removed)
+            SaveEmails(emails);
+        return removed;
+
+    }
+
+}
