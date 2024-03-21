6fa028cdeb9b41791afc6e0557d45f9b64470591
Sydney <github@shonx.net>
2024-03-21 02:14:03 -0400

    3rd revision

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ public class Program
     public const string WEATHER_API_PROVIDER = "https://provider.weather.shonx.net/";
     public const string WEATHER_BACKEND_PROVIDER = "https://backend.weather.shonx.net/";
 
-    public const int REVISION = 2;
+    public const int REVISION = 3;
     public static void Main(string[] args)
     {
         Console.WriteLine("Running revision " + REVISION);
