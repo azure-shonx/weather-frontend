4911026a64b8ae49be2ded85ba04ab7789a50dca
Sydney <github@shonx.net>
2024-03-20 20:44:00 -0400

    Update URLs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 public class Program
 {
-    public const string WEATHER_API_PROVIDER = "http://provider.weather.shonx.net/";
-    public const string WEATHER_BACKEND_PROVIDER = "http://backend.weather.shonx.net/";
+    public const string WEATHER_API_PROVIDER = "https://provider.weather.shonx.net/";
+    public const string WEATHER_BACKEND_PROVIDER = "https://backend.weather.shonx.net/";
     public static void Main(string[] args)
     {
         new WebHandler(args);
