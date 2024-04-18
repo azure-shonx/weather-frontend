9889cea15966e3f09a336876a018b6b9b7fa8691
Sydney <github@shonx.net>
2024-04-18 01:54:54 -0400

    More minor edits.

--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,7 @@ using net.shonx.weather.frontend.web;
 
 public class Program
 {
-    public const string WEATHER_BACKEND_PROVIDER = "https://backend.weather.shonx.net/";
+    public const string BACKEND = "https://backend.weather.shonx.net/";
 
     public static void Main(string[] args)
     {
--- src/net/shonx/weather/frontend/web/HtmlHandler.cs
+++ src/net/shonx/weather/frontend/web/HtmlHandler.cs
@@ -1,20 +1,20 @@
+namespace net.shonx.weather.frontend;
+
 using System.Text;
 using net.shonx.weather.backend;
 using Newtonsoft.Json;
-
-namespace net.shonx.weather.frontend;
 
 internal static class HtmlHandler
 {
     private static readonly HttpClient httpClient = new();
 
-    internal static async Task<WeatherForecast> GetWeather(int zipcode)
+    internal static async Task<WeatherForecast?> GetWeather(int zipcode)
     {
-        using HttpResponseMessage response = await httpClient.GetAsync(Program.WEATHER_BACKEND_PROVIDER + "weather/get/" + zipcode);
+        using HttpResponseMessage response = await httpClient.GetAsync(Program.BACKEND + "weather/get/" + zipcode);
         string apiResponse = await response.Content.ReadAsStringAsync();
         if (string.IsNullOrEmpty(apiResponse))
             throw new NullReferenceException("Provider response is empty. Is it online?");
-        return JsonConvert.DeserializeObject<WeatherForecast>(apiResponse) ?? throw new NullReferenceException();
+        return JsonConvert.DeserializeObject<WeatherForecast>(apiResponse);
     }
 
     internal static async Task<bool> WriteEmail(Email email, bool isSubbing)
@@ -22,7 +22,7 @@ internal static class HtmlHandler
         string json = JsonConvert.SerializeObject(email);
         StringContent data = new(json, Encoding.UTF8, "application/json");
 
-        string URL = isSubbing ? Program.WEATHER_BACKEND_PROVIDER + "emails/add/" : Program.WEATHER_BACKEND_PROVIDER + "emails/remove/";
+        string URL = isSubbing ? Program.BACKEND + "emails/add/" : Program.BACKEND + "emails/remove/";
 
         HttpRequestMessage request = new(HttpMethod.Put, URL)
         {
